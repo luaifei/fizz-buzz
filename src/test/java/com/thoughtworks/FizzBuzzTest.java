586c93df7100183a465a42586e54cba7475c6f21
luaifei <lu.aifei@thoughtworks.com>
2020-01-11 15:07:47 +0800

    [3] Lu Aifei: Fix mockito的引用错误；给定数字是5的倍数时，当报数的时候，应该返回Buzz。

--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -11,4 +11,12 @@ public class FizzBuzzTest {
         FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("Fizz", fizzBuzz.report(inputNumber));
     }
+
+    @Test
+    void should_return_Buzz_when_fizzBuzz_given_number_is_divisible_by_5() {
+        int inputNumber = 5;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Buzz", fizzBuzz.report(inputNumber));
+    }
+
 }
