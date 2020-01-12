d0637ad9198cac1accdb4093520c508a37a42781
luaifei <lu.aifei@thoughtworks.com>
2020-01-12 21:07:38 +0800

    [5] Lu Aifei: 添加输出结果

--- src/main/java/com/thoughtworks/FuzzBuzz.java
+++ src/main/java/com/thoughtworks/FuzzBuzz.java
@@ -1,6 +1,12 @@
 package com.thoughtworks;
 
+import java.util.stream.IntStream;
+
 public class FuzzBuzz {
+    public static void main (String ... args) {
+        FuzzBuzz fuzzBuzz = new FuzzBuzz();
+        IntStream.range(1, 121).forEach(num -> System.out.println(fuzzBuzz.report(num)));
+    }
     public String report(int num) {
         String result = "";
         if (num % 3 == 0 && (!isContains(num, "5") || isContains(num, "7"))) {
