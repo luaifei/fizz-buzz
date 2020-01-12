1b2a96a10ba5e918bbcd1e0f44fd0c0193f36882
luaifei <lu.aifei@thoughtworks.com>
2020-01-12 22:13:45 +0800

    [5] Lu Aifei: 完成计时：15分钟

--- src/main/java/com/thoughtworks/FizzBuzz.java
+++ src/main/java/com/thoughtworks/FizzBuzz.java
@@ -3,18 +3,22 @@ package com.thoughtworks;
 public class FizzBuzz {
     public String report(int num) {
         String result = "";
-        if (num % 3 == 0) {
+        if (num % 3 == 0 && (!isContains(num, "5") || isContains(num, "7"))) {
             result += "Fizz";
         }
-        if (num % 5 == 0) {
+        if (num % 5 == 0 && !isContains(num, "7")) {
             result += "Buzz";
         }
         if (num % 7 == 0) {
             result += "Whizz";
         }
-        if (String.valueOf(num).contains("3")) {
+        if (isContains(num, "3") && (!isContains(num, "5") || isContains(num, "7"))) {
             result = "Fizz";
         }
         return result.isEmpty() ? String.valueOf(num) : result;
+    }
+
+    public boolean isContains(int num, String str) {
+        return String.valueOf(num).contains(str);
     }
 }
--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -3,6 +3,8 @@ package com.thoughtworks;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 public class FizzBuzzTest {
     @Test
@@ -31,7 +33,8 @@ public class FizzBuzzTest {
 
     @Test
     void should_return_FizzBuzz_given_input_is_divisible_by_3_and_5() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
+        FizzBuzz fizzBuzz = spy(FizzBuzz.class);
+        when(fizzBuzz.isContains(15, "5")).thenReturn(false);
         assertEquals("FizzBuzz", fizzBuzz.report(15));
     }
 
@@ -43,7 +46,32 @@ public class FizzBuzzTest {
 
     @Test
     void should_return_Fizz_given_input_contains_3_and_is_divisible_by_5() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
+        FizzBuzz fizzBuzz = spy(FizzBuzz.class);
+        when(fizzBuzz.isContains(35, "5")).thenReturn(false);
         assertEquals("Fizz", fizzBuzz.report(35));
+    }
+
+    @Test
+    void should_return_Buzz_given_input_contains_5_and_is_divisible_by_3() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Buzz", fizzBuzz.report(15));
+    }
+
+    @Test
+    void should_return_BuzzWhizz_given_input_contains_5_and_3_and_is_divisible_by_7() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("BuzzWhizz", fizzBuzz.report(35));
+    }
+
+    @Test
+    void should_return_Fizz_given_input_contains_5_and_7_and_3() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Fizz", fizzBuzz.report(357));
+    }
+
+    @Test
+    void should_return_Fizz_given_input_contains_7_and_5_and_is_divisible_by_3() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Fizz", fizzBuzz.report(375));
     }
 }
