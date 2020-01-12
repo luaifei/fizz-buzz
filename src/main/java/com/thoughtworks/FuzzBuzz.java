1847b8e973c5ae687c7263b011b01387c17a4c8f
luaifei <lu.aifei@thoughtworks.com>
2020-01-12 21:30:15 +0800

    [5] Lu Aifei: 重新练习：17分钟完成

---
+++ src/main/java/com/thoughtworks/FuzzBuzz.java
@@ -0,0 +1,24 @@
+package com.thoughtworks;
+
+public class FuzzBuzz {
+    public String report(int num) {
+        String result = "";
+        if (num % 3 == 0 && (!isContains(num, "5") || isContains(num, "7"))) {
+            result += "Fizz";
+        }
+        if (num % 5 == 0 && !isContains(num, "7")) {
+            result += "Buzz";
+        }
+        if (num % 7 == 0) {
+            result += "Whizz";
+        }
+        if (isContains(num, "3") && (!isContains(num, "5") || isContains(num, "7"))) {
+            result = "Fizz";
+        }
+        return result.isEmpty() ? String.valueOf(num) : result;
+    }
+
+    public boolean isContains(int num, String str) {
+        return String.valueOf(num).contains(str);
+    }
+}
